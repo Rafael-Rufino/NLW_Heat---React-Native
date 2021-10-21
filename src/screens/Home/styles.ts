3636973e8a32791b1e5c903fe12345d01e3d0310
RafaelRufino <rafaelrufino.52@gmail.com>
2021-10-21 03:27:50 -0300

    configurando a estrutura do projeto

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    fontSize: 18,
+    backgroundColor: COLORS.BLACK_SECONDARY,
+    color: COLORS.WHITE,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
