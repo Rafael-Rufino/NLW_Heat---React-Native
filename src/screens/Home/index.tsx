baf37ff090bcc17e565a1346593ea2aafcb7448e
RafaelRufino <rafaelrufino.52@gmail.com>
2021-10-22 02:22:49 -0300

    Finalizando aplicação com react native

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, KeyboardAvoidingView, Platform } from "react-native";
 import { styles } from "./styles";
+import { Header } from "../../components/Header";
+import { MessageList } from "../../components/MessageList";
+import { SignInBox } from "../../components/SignInBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
+import { useAuth } from "../../hooks/auth";
+
 export default function Home() {
+  const { user } = useAuth();
   return (
-    <View style={styles.container}>
-      <Text>Hello React native</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+        {user ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
   );
 }
