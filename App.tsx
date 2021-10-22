0487caab14f992b09b0e6e2c456bd708217d84af
RafaelRufino <rafaelrufino.52@gmail.com>
2021-10-22 02:21:43 -0300

    configurando aplicação

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-
+import { AuthProvider } from "./src/hooks/auth";
 import AppLoading from "expo-app-loading";
 
 import Home from "./src/screens/Home";
 
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,9 +23,13 @@ export default function App() {
   }
 
   return (
-    <>
-      <StatusBar style="light" />
+    <AuthProvider>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Home />
-    </>
+    </AuthProvider>
   );
 }
